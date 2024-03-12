7ade5d45a04cbd6bff01e6d79a6f82478118d0b4
ConsoleWarrior <oleg.minichev@yandex.ru>
2024-03-12 22:10:04 +0300

    добавили реализацию репозитория в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1,"Белый клык"),
+            new Book(2,"Алая чума"),
+            new Book(3,"Мартин Иден"),
+            new Book(1,"Черный клык"),
+
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
+
